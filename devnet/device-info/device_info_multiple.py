e6779d16735e3c188099054f803de5b30af0ae2c
guobwangcyber <spookfox@163.com>
2022-04-01 22:58:12 +0800

    commit

--- devnet/device-info/device_info_multiple.py
+++ devnet/device-info/device_info_multiple.py
@@ -1,5 +1,16 @@
-with open('device-multiple.txt.txt','r') as f:
-    file_info = f.readline().strip()
-    print(file_info)
-    device_info = file_info.split(',')
-    print(device_info)
+with open('device-multiple.txt','r') as f:
+    # file_info = list()
+    # for line in f:
+        # # file_info = line.strip().split(',')
+        # file_info.append(line.strip().split(','))
+        # print(file_info)
+#     info = f.readline()
+# #     print(info)
+#         info2 = f.readlines()
+#         print(info2)
+#         for i in info2:
+#             print(i.strip().split(','))
+# #
+# # with open('show-run.txt','r') as e:
+# #     for i in e:
+# #         print(i.strip().split('\n'))
